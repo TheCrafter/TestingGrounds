0e06348b52c6a53cfb8511eec66f97203dae87ae
TheCrafter <dimitrisvlh@gmail.com>
2018-02-26 01:49:24 +0200

    Create ChooseNextWaypoint C++ file

---
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+    int32 Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+    UE_LOG(LogTemp, Warning, TEXT("Waypong index: %i"), Index)
+    return EBTNodeResult::Succeeded;
+}
